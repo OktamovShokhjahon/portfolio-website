d10783f74defe5ebcd850868bb5200c70c9ad955
Shohjahon <oktamovshokhjahon@gmail.com>
2024-09-15 15:02:59 +0500

    start

---
+++ src/lib/content/portfolio.ts
@@ -0,0 +1,40 @@
+/*
+ * =========================
+ * AUTHOR INFO
+ * =========================
+ */
+
+import { StringKeyValueType } from '../types';
+
+export const socialLinks: StringKeyValueType = {
+  facebook: 'https://www.facebook.com/vatsalsinghkv',
+  instagram: 'https://www.instagram.com/vatsalsinghkv',
+  twitter: 'https://twitter.com/vatsalsinghkv',
+  github: 'https://github.com/vatsalsinghkv',
+  linkedin: 'https://www.linkedin.com/in/vatsalsinghkv/',
+};
+
+export const author = {
+  name: 'Oktamov Shohjahon',
+  email: 'oktamovshohjahon596@gmail.com',
+};
+
+export const seoData = {
+  title: 'Oktamov Shohjahon | Full-Stack Developer',
+  description:
+    'Vatsal Singh is a front-end developer who specializes in building (and occasionally designing) exceptional visual interfaces.',
+  author: author.name,
+  image:
+    'https://user-images.githubusercontent.com/68834718/217467445-0b0d2d63-f8ad-4702-8474-814eb2cbb3be.png',
+  url: 'https://vatsalsinghkv.vercel.app/',
+  keywords: [
+    'Shohjahon',
+    'Oktamov Shohjahon',
+    "O'ktamov Shohjahon",
+    'oktamovshokhjahon@gmail.com',
+    '@ShohjahonDev',
+    'Shohjahon Portfolio ',
+    'Oktamov Shohjahon Portfolio',
+    'FullStack developer portfolio',
+  ],
+};
